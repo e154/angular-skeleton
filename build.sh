5a0e78f819fb90dd27778f516d6b35aa1c1fa220
delta54 <support@e154.ru>
2015-11-06 22:40:53 +0600

    add bash script

---
+++ build.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+
+function do_init() {
+
+	sudo npm install -g bower
+	npm install
+	bower install
+	gulp
+}
+
+function do_clear() {
+
+	rm -frd node_modules
+	rm -frd static_source/bower_components
+	rm -frd static_source/css
+	rm -frd static_source/js
+}
+
+case "$1" in
+    init)
+    do_init
+    ;;
+    clear)
+    do_clear
+    ;;
+    *)
+    echo "Usage: $0 init|clear" >&2
+    exit 3
+    ;;
+esac
